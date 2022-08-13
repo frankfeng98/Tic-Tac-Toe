8a767989efe3b16ab7f6ff642044abaa265c2706
Frank Feng <frankfeng1998@gmail.com>
2022-08-12 23:16:10 -0500

    Add the name feature

--- source.js
+++ source.js
@@ -1,5 +1,5 @@
 const player = (name, sign, turn) => {
-    const _playerName = name;
+    let _playerName = name;
     const _playerSign = sign;
     let _playerTurn = turn;
     const play = (index, playerSign) => {
@@ -17,6 +17,10 @@ const player = (name, sign, turn) => {
         _playerTurn = !_playerTurn;
     }
 
+    const setName = (name) => {
+        _playerName = name;
+    }
+
     const playerName = () => {
         return _playerName;
     }
@@ -25,7 +29,8 @@ const player = (name, sign, turn) => {
         playTurn,
         playSign,
         setTurn,
-        playerName
+        playerName,
+        setName
     };
 }
 
@@ -34,6 +39,10 @@ const gameBoard = (() => {
     const fields = document.querySelectorAll(".field");
     const title = document.querySelector('.title');
     const winMsg = document.createElement('h3');
+    const restartElement = document.querySelector('.restart');
+    const modal_restart = document.querySelector(".modal-restart");
+    const modal_start = document.querySelector(".modal-start");
+    const start = document.querySelector(".start-name");
 
     const getBoard = (index) => _board[index];
     const setBoard = (index, sign) => {
@@ -50,7 +59,19 @@ const gameBoard = (() => {
         if (checkWin(_player.playSign())){
             popUpMsg(_player.playerName());
         }
+        if (checkDraw()){
+            popUpMsg("Everyone");
+        }
 
+    }
+    const checkDraw = () => {
+        let temp = 0;
+        _board.forEach(field => {
+            if (field === 'O' || field === 'X'){
+                temp++;
+            }
+        })
+        return temp === 9;
     }
     const prepareBoard = (_player1, _player2) => {
         fields.forEach(field => {
@@ -64,7 +85,40 @@ const gameBoard = (() => {
                 }
             })
         });
+        restartElement.addEventListener('click', restart);
+        start.addEventListener('click', () => {
+            enterName(_player1, _player2);
+        })
     }
+
+    const enterName = (_player1, _player2) => {
+        const start = document.querySelector(".start-game");
+
+        modal_start.classList.add('show-modal');
+        window.addEventListener("click", (e) => {
+            if (e.target === modal_start){
+                modal_start.classList.remove('show-modal');
+                showName(_player1, _player2);
+            }
+        })
+
+        start.addEventListener('click', () => {
+            modal_start.classList.remove('show-modal');
+            showName(_player1, _player2);
+        })
+    }
+
+    const showName = (_player1, _player2) => {
+        const left = document.querySelector(".left");
+        const right = document.querySelector(".right");
+        const player1_name = document.querySelector("#player1").value;
+        const player2_name = document.querySelector("#player2").value;
+        left.textContent = player1_name;
+        right.textContent = player2_name;
+        _player1.setName(player1_name);
+        _player2.setName(player2_name);
+    }
+
     const checkWin = (sign) => {
         if (_board[0] === sign && _board[1] === sign && _board[2] === sign
             || _board[3] === sign && _board[4] === sign && _board[5] === sign
@@ -80,28 +134,23 @@ const gameBoard = (() => {
         }
     }
     const popUpMsg = (winName) => {
-        const modal = document.querySelector(".modal");
-        console.log(modal);
         const closeButton = document.querySelector(".close-button");
-        modal.classList.add('show-modal');
-        console.log(modal);
-        closeButton.addEventListener("click", () => {
-            modal.classList.remove('show-modal');
-        });
+        modal_restart.classList.add('show-modal');
         window.addEventListener("click", (e) => {
-            if (e.target === modal){
-                modal.classList.remove('show-modal');
+            if (e.target === modal_restart){
+                modal_restart.classList.remove('show-modal');
+                restart();
             }
         })
         const winMsg = document.querySelector("h1");
         winMsg.textContent = `${winName} is the Winner!`;
-        restart();
     }
     const restart = () => {
         _board = new Array(9);
         fields.forEach(field => {
             field.textContent = '';
         })
+        modal_restart.classList.remove('show-modal');
     }
     return {
         setBoard,
